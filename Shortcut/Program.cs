d98142622d4738462fb9267cb539f0e4f3cd717a
nattapon <nattapon@sycapt.com>
2025-08-25 20:29:03 +0700

    Allow one instance

--- Shortcut/Program.cs
+++ Shortcut/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Shortcut
 {
@@ -12,9 +13,26 @@ namespace Shortcut
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Shortcut());
+            bool createdNew;
+            // Use a unique name for your mutex to prevent conflicts
+
+            using (Mutex mutex = new Mutex(true, "Shortcut-48B4-4D4B", out createdNew))
+            {
+                if (createdNew)
+                {
+                    // This is the first instance, run the application
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new Shortcut());
+                }
+                else
+                {
+                    // Another instance is already running, show a message and exit
+                    MessageBox.Show("Another instance of this application is already running.", "Application Running", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+            
         }
     }
 }
